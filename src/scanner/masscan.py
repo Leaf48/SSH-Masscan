b593103d67ab3d63fb0ee4ed3de48c5fee57abef
leaf48 <watavita2004@gmail.com>
2024-10-04 19:29:18 -0700

    feat: masscan wrapper

---
+++ src/scanner/masscan.py
@@ -0,0 +1,22 @@
+import subprocess
+import json
+
+
+class Masscan:
+    @staticmethod
+    def scan(ipv4: str, ports: str, maxrate: str):
+        command = [
+            "masscan",
+            "-oJ",
+            "-",
+            ipv4,
+            "-p",
+            ports,
+            "--max-rate",
+            maxrate,
+        ]
+        result = subprocess.run(command, capture_output=True, text=True)
+        scan_result = json.loads(result.stdout)
+        print(scan_result)
+
+        return scan_result
